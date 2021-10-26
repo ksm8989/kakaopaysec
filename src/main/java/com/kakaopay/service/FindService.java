2e782d3f28828c949fb83ec27d6d65f0161ece33
jhyh <jhyh@tosspayments.com>
2021-10-26 18:05:11 +0900

    refactor: 불필요한 public 접근제어 제거

--- src/main/java/com/kakaopay/service/FindService.java
+++ src/main/java/com/kakaopay/service/FindService.java
@@ -7,14 +7,14 @@ import com.kakaopay.model.transaction.Transaction;
 import java.util.Map;
 
 public interface FindService {
-    public Map<String, Account> getAllAccounts();
+    Map<String, Account> getAllAccounts();
 
-    public Map<String, Branch> getAllBranches();
+    Map<String, Branch> getAllBranches();
 
-    public Map<String, Transaction> getAllTransactions();
+    Map<String, Transaction> getAllTransactions();
 
-    public Account getAccount(String acctNo);
+    Account getAccount(String acctNo);
 
-    public Branch getBranch(String placeCd);
+    Branch getBranch(String placeCd);
 
 }
